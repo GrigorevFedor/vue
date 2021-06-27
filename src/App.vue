c415f4b9de50ef992c88fa3ea1f9ebcb920ef551
popapauka <gfavlg@yandex.ru>
2021-06-27 17:04:28 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Calc />
+    <HelloWorld 
+      msg="message" 
+      :myArr= "arr"
+    />
+    Text : {{ message }} длина {{ message.length } }
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+   HelloWorld,
+   Calc
+  },
+  data: () =>({
+    message: "Ваша реклама12321",
+    arr: [1,2,3,4,5]
+  })
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
